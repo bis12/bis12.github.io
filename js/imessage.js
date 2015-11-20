c854637f7f1fe0f6119a42e5eeacc4f177d85809
Brian Stack <bstack01@gmail.com>
2015-11-20 13:05:46 -0500

    Being punchcard viz

--- js/imessage.js
+++ js/imessage.js
@@ -1,3 +1,51 @@
+var punchcard = function(args) {
+    var el = d3.select(args.bindto);
+    var margin = {top: 20, right: 10, bottom: 20, left: 50};
+    var width = el[0][0].offsetWidth - margin.left - margin.right;
+    var height = width * (1/1.6) - margin.top - margin.bottom;
+
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    var svg = el.append('svg')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+                .append('g')
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.linear().domain([0, 25]).range([0, width]);
+    var y = d3.scale.linear().domain([0, 6]).range([0, height]);
+
+    yAxis = d3.svg.axis().ticks(7).scale(y).orient('left').tickFormat( function(d, i) { return days[i]; });
+    svg.append('g').attr("class", "x axis").call(yAxis);
+
+    for (var i in days) {
+        svg.append('g')
+           .attr("transform", "translate(0," + y(i) + ")")
+           .attr("class", "x axis")
+           .call(d3.svg.axis().scale(x).ticks(24).tickFormat(''));
+    }
+
+    d3.text(args.data, function(error, text) {
+        if (error) return console.warn(error);
+        var data = d3.csv.parseRows(text)
+
+        svg.append('g')
+           .selectAll('g')
+           .data(data)
+           .enter()
+           .append('g')
+           .selectAll('g')
+           .data( function(d,i,j) { return d; } )
+           .enter()
+           .append('circle')
+           .attr('fill', '#333')
+           .attr('class','hover')
+           .attr('cy', function(d,i,j) { return y(j); })
+           .attr('cx', function(d,i,j) { return x(i + 1); })
+           .attr('r', function(d,i,j) { return +d * 0.02; });
+    });
+}
+
 $(document).ready(function() {
 
     // Plot of people in chat room
@@ -43,5 +91,11 @@ $(document).ready(function() {
                 label: 'Messages per week',
             }
         }
+    });
+
+    // Punchcard for text times
+    punchcard({
+        bindto: '#imessage-fig3>div',
+        data: '/data/punchcard.csv'
     });
 });
