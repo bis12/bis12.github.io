bdc1c562017f239cb6b6a06254a8c50d3c8807ea
Brian Stack <bstack@mozilla.com>
2017-03-27 15:50:38 -0700

    Fix letsencrypt script

--- extra/letsencrypt/script.sh
+++ extra/letsencrypt/script.sh
@@ -1,9 +1,11 @@
-export secret_url="taskcluster/secrets/v1/secret/repo:github.com/imbstack/imbstack.github.io"
-export AWS_ACCESS_KEY_ID=$(curl ${secret_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_ACCESS_KEY_ID"]')
-export AWS_SECRET_ACCESS_KEY=$(curl ${secret_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_SECRET_ACCESS_KEY"]')
-export CF_DISTRIBUTION_ID=$(curl ${secret_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["CF_DISTRIBUTION_ID"]')
-certbot --agree-tos -a certbot-s3front:auth --renew-by-default --text \
-  --certbot-s3front:auth-s3-bucket imbstack.com \
+export SECRET_URL="taskcluster/secrets/v1/secret/repo:github.com/imbstack/imbstack.github.io"
+export AWS_ACCESS_KEY_ID=$(curl ${SECRET_URL} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_ACCESS_KEY_ID"]')
+export AWS_SECRET_ACCESS_KEY=$(curl ${SECRET_URL} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_SECRET_ACCESS_KEY"]')
+export CF_DISTRIBUTION_ID=$(curl ${SECRET_URL} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["CF_DISTRIBUTION_ID"]')
+export CF_DOMAIN=$(curl ${SECRET_URL} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["DOMAIN"]')
+export CERT_EMAIL=$(curl ${SECRET_URL} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["CERT_EMAIL"]')
+certbot --agree-tos --email $CERT_EMAIL -na certbot-s3front:auth --keep-until-expiring --text \
+  --certbot-s3front:auth-s3-bucket $CF_DOMAIN \
   -i certbot-s3front:installer \
   --certbot-s3front:installer-cf-distribution-id $CF_DISTRIBUTION_ID \
-  -d imbstack.com
+  -d $CF_DOMAIN
