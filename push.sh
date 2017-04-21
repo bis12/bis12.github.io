f4070d6babb25c134ecf5ffeb50a60663d31d228
Brian Stack <bstack@mozilla.com>
2017-04-20 18:43:56 -0700

    Debug

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
+set -x
 export secret_url="taskcluster/secrets/v1/secret/repo:github.com/imbstack/imbstack.com"
 export AWS_ACCESS_KEY_ID=$(curl ${secret_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_ACCESS_KEY_ID"]')
 export AWS_SECRET_ACCESS_KEY=$(curl ${secret_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_SECRET_ACCESS_KEY"]')
