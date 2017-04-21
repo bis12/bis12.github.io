c5c709a339ae6d19ad1fc108c485821868ca952a
Brian Stack <bstack@mozilla.com>
2017-04-20 17:44:11 -0700

    Fix env location

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 export secret_url="taskcluster/secrets/v1/secret/repo:github.com/imbstack/imbstack.github.io"
 export AWS_ACCESS_KEY_ID=$(curl ${secret_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_ACCESS_KEY_ID"]')
 export AWS_SECRET_ACCESS_KEY=$(curl ${secret_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["AWS_SECRET_ACCESS_KEY"]')
+export JEKYLL_ENV=production
 aws s3 sync --delete _site s3://imbstack.com
